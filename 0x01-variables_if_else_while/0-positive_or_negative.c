4c4dfb495264f5f75ee4261f257c381de475721f
afarizap <afarizap@gmail.com>
2020-02-06 19:27:13 +0000

    convergence numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/* and more more headers goes there */
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (a > 0)
+if (a > 0)
 	{
 		printf("%d is positive");
 	} else if (a < 0)

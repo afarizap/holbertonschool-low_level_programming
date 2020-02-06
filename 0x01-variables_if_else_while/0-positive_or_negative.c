a6ecc573cf83e4a770c374bf703eec2c22c14abc
afarizap <afarizap@gmail.com>
2020-02-06 19:53:46 +0000

    convergence 6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%i is positive", n);
+		printf("%d is positive\n", n);
 	} else if (n < 0)
 	{
-		printf("%i is negative", n);
+		printf("%d is negative\n", n);
 	} else
 	{
-		printf("%i is zero", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }

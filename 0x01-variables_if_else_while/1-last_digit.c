0d7872cf6406ab309f808ac023d911bb14db9a2c
afarizap <afarizap@gmail.com>
2020-02-06 21:23:40 +0000

    warning and errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
-	printf("Last digit of %d is %i and is", n, a);
+	printf("Last digit of %i is %i and is", n, a);
 	if (a > 5)
 	{
 		printf(" greater than 5\n");
-	} else if (a < 6)
-	{
-		printf(" less than 6 and not 0\n");
 	} else if (a == 0)
 	{
 		printf(" and is 0\n");
+	} else if (a < 6 && a != 0)
+	{
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }

810ecb9fc8ff94cd680fd3a32753d01634ccba39
afarizap <afarizap@gmail.com>
2020-02-07 23:08:43 +0000

    sons and parents

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 					putchar(' ');
 					putchar(c);
 					putchar(d);
-					if (a <= '9' && b < '8')
+					if (b < '9' && c <= '9' && d <= '9')
 					putchar(',');
 					putchar(' ');
 				}

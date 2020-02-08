27e40e5c9f7614be2a4bd09a086225b7fb6be24b
afarizap <afarizap@gmail.com>
2020-02-08 15:37:10 +0000

    final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,18 +15,21 @@ int main(void)
 	for (a = '0'; a <= '9'; a++)
 		for (b = '0'; b <= '9'; b++)
 			for (c = a; c <= '9'; c++)
-				for (d = b + 1; d <= '9'; d++)
+				for (d = b; d <= '9'; d++)
 				{
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(c);
-					putchar(d);
+					if (!(b == d && a == c))
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+					}
 					if (!(a == '9' && b == '8'))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 	putchar('\n');
 	return (0);

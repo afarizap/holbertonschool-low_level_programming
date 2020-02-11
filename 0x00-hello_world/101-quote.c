089b92e9270ba713a7e24b27891ed0e088b30b18
afarizap <afarizap@gmail.com>
2020-02-11 18:38:31 +0000

    last modf 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print stderr "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+		printf(\n);
+	return(1)
+		}

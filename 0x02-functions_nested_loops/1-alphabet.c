034194f000799faabb1adf89cdd662139e882d7a
afarizap <afarizap@gmail.com>
2020-02-11 14:15:27 +0000

    low level headers

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void print_alphabet(void);
+
+char c [27] = "abcdefghijklmnopqrstuvxyz"
+
+	for (int c = a; c < z; c++)
+	{
+		_putchar(c);
+		_putchar(\n);
+	}

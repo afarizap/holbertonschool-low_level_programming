2b10c3d7ae96cb69ad6fb76f05a547e95c5d102e
afarizap <afarizap@gmail.com>
2020-02-13 22:50:33 +0000

    fix some points

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * void more_numbers - print numbers from 0 to 9
+ *more_numbers - print numbers from 0 to 9
  *
- * Return: 1 - gives a digit
+ *
  *
  *
  */
 void more_numbers(void)
 {
-	int a = '0';
-	int b = '0';
+	int a;
+	int b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-	       	for (b = '0'; b < '15';b++)
+		for (b = 0; b < 15; b++)
 		{
-			if (b > '9')
-				_putchar(b/10 + 48);
-			_putchar(b%10 +48);
+			if (b > 9)
+				_putchar(b / 10 + 48);
+			_putchar(b % 10 + 48);
 		}
-
 		_putchar('\n');
 	}
 }

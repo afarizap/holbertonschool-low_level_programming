ce12a23d5b8b68475ceff02af0ad83451df0c30d
afarizap <afarizap@gmail.com>
2020-02-13 22:28:24 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - buzz fizz up to 100
+ *Return: numbers with fizz and buzz
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

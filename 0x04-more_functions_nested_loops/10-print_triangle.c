95889241fb4394baedf13501a58427a6e8dd94f5
afarizap <afarizap@gmail.com>
2020-02-14 17:43:59 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * print_triangle - prints a number triangles if positive number.
+ *@size: - number of times line is printed
+ */
 
 void print_triangle(int size)
 {
+	int l;
+	int i;
+	int c;
+
+	for (l = 0; l < size; l++)
+	{
+		for (i = 1; i < (size - l); i++)
+		{
+			_putchar(' ');
+		}
+		for (c = 0; c <= l; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}

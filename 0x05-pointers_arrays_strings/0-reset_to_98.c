7244c95a12b4ae1d23a88b84b0dcd436590268d6
afarizap <afarizap@gmail.com>
2020-02-18 16:56:15 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - reset value of an int variable to 98
+ *
+ * @n : pointer variable set to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}

587cd7ddc89e886b51e5ff51f733f7511dbd5255
afarizap <afarizap@gmail.com>
2020-02-19 01:19:11 +0000

    put tail of string, if odd the 1 less. fix null

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,13 @@ void puts_half(char *str)
 	{
 		x++;
 	}
-	x--;
 	if (x % 2 == 0)
 	{
 		w = x / 2;
 		while (str[w] != '\0')
 		{
-			_putchar(str[w]);
 			w++;
+			_putchar(str[w]);
 		}
 	}
 	else

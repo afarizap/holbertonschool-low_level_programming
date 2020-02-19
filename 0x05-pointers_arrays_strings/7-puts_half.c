128ed13d727830f37095b414b4075f16f5d5872e
afarizap <afarizap@gmail.com>
2020-02-19 01:06:37 +0000

    put tail of string, if odd then 1 less

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * puts_half - putchar a string with 1 memory of distance.
+ *
+ *@str: string
+ */
+void puts_half(char *str)
+{
+	int w = 0;
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	if (x % 2 == 0)
+	{
+		w = x / 2;
+		while (str[w] != '\0')
+		{
+			_putchar(str[w]);
+			w++;
+		}
+	}
+	else
+	{
+		w = (x - 1) / 2;
+		while (str[w] != '\0')
+		{
+			w++;
+			_putchar(str[w]);
+		}
+	}
+	_putchar('\n');
+}

46bae5c3dda01bf6bc4a9ea69e5f71aa026d165d
afarizap <afarizap@gmail.com>
2020-02-19 02:05:56 +0000

    final correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,27 +11,18 @@ void puts_half(char *str)
 	int x = 0;
 
 	while (str[x] != '\0')
-	{
 		x++;
-	}
-	x--;
 	if (x % 2 == 0)
-	{
 		w = x / 2;
-	}
 	if (x % 2 == 1)
-	{
 		w = (x - 1) / 2;
-	}
 	while (str[w] != '\0')
-	{
 		if (w != 0)
 		{
-		_putchar(str[w]);
-		w++;
+			_putchar(str[w]);
+			w++;
 		}
 		else
 			w++;
-	}
 	_putchar('\n');
 }

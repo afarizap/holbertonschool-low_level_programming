1f1bb8eea6d50b15905a40b4101a3885a86241d8
afarizap <afarizap@gmail.com>
2020-02-21 17:07:44 +0000

    betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,31 +3,29 @@
 /**
  * *cap_string - make lowercase to upercase on a string
  * @s: string to make uppercase
- * Return: variable x
+ * Return: variable s
  */
 char *cap_string(char *s)
 {
-        int ns;
-        int ne;
-        char e[] = {'}', '{', ')', '(', '"', '?', '!', '.', ';', ',', ' ', '\t', '\n'};
-
-        if (s[0] >= 'a' && s[0] <= 'z')
-        {
-                *s -= 32;
-        }
-        for (ns = 0; s[ns] ; ns++)
-        {
-                for (ne = 0; e[ne] ; ne++)
-                {
-                        if (s[ns] == e[ne])
-                        {
-                                if (s[ns + 1] >= 97 && s[ns + 1] <= 122)
-                                {
-                                        *(s + ns + 1) -= 32;
-                                        break;
-                                }
-                        }
-                }
-        }
-        return (s);
+	int ns;
+	int ne;
+	char e[] = {'}', '{', ')', '(', '"', '?', '!', '.', ';',
+		    ',', ' ', '\t', '\n'};
+	if (s[0] >= 'a' && s[0] <= 'z')
+		*s -= 32;
+	for (ns = 0; s[ns] ; ns++)
+	{
+		for (ne = 0; e[ne] ; ne++)
+		{
+			if (s[ns] == e[ne])
+			{
+				if (s[ns + 1] >= 97 && s[ns + 1] <= 122)
+				{
+					*(s + ns + 1) -= 32;
+					break;
+				}
+			}
+		}
+	}
+	return (s);
 }

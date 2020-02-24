64474d7c4f2b7a491b7a158ffbfdba9fd01fded2
afarizap <afarizap@gmail.com>
2020-02-24 21:26:31 +0000

    3+2

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+	int b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+			if (s[a] == accept[b])
+			{
+				return (s + a);
+			}
+	}
+	return (0);
+}

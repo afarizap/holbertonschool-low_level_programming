6db02055a9e2d725db02926128e03e79c13aca73
afarizap <afarizap@gmail.com>
2020-02-24 23:02:18 +0000

    pasword

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - check the code for Holberton School students.
+ *@haystack:
+ *@needle:
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+			if (haystack[a] == needle[b])
+			{
+				haystack = haystack[a];
+				return (haystack);
+			}
+	}
+	return (0);
+}

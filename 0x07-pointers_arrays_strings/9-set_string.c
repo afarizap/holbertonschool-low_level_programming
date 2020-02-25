625558c3a37e99f3640f7a7c3309db6759efc3d9
afarizap <afarizap@gmail.com>
2020-02-25 03:24:31 +0000

    last

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - string to a string.
+ * @s: string double
+ * @to: string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

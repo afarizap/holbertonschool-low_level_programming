f946419014de1f1e29cededd7fa4d93b4af6aa19
afarizap <afarizap@gmail.com>
2020-02-26 19:14:08 +0000

    recursion string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+		_puts_recursion(s);
+	if (*s == '\0')
+		_putchar('\n');
+}

9fcc25a377f2aeaf99c1ef06dc4fc875f786ad28
afarizap <afarizap@gmail.com>
2020-03-03 23:08:35 +0000

    concatenated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,20 +3,21 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * *str_concat - concatenate two strings into one and asign memory.
+ *@s1: string 1
+ *@s2: string 2
+ * Return: retunr concatenated strings or null.
  */
 char *str_concat(char *s1, char *s2)
 {
 	int a = 0;
 	char *s;
 	int b = 0;
-	int aa = 0;
-	int bb = 0;
+	int a2 = 0;
+	int b2 = 0;
 	int ab = 0;
 
-	if (s1 == '\0' && s2 == '\0')
+	if (s1 == '\0' || s2 == '\0')
 		return ('\0');
 
 	for (; s1[a] != '\0'; a++)
@@ -25,11 +26,13 @@ char *str_concat(char *s1, char *s2)
 	for (; s2[b] != '\0'; b++)
 	{
 	}
-	ab = a + b;
+	ab = a + b + 1;
 	s = malloc(sizeof(char) * ab);
-	for (;aa <= a; aa++, s1++, ++s)
-		s = s1;
-	for (;bb <= b; bb++, s2++, ++s)
-		s = s2;
+	if (s == NULL)
+		return (NULL);
+	for (; a2 < a; a2++)
+		s[a2] = s1[a2];
+	for (; b2 < b; b2++)
+		s[a2 + b2] = s2[b2];
 	return (s);
 }

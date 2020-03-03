237926e666e8e94b0b3a70be41166b4214f245f7
afarizap <afarizap@gmail.com>
2020-03-03 17:44:12 +0000

    f

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 
 /**
- *
+ * *create_array - creates an array of a size and filled with char c
+ * @size: size of the string
+ * @c: character in the string
  */
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, __attribute__((unused))char c)
 {
-	int i;
-	char *s[size];
+	unsigned int i = 0;
+	char *s;
 
 	if (size == 0)
-		return ('\0');
-	else
-		for (i = 0; i < size; i++)
-			s[i] = c;
+		return (NULL);
+	s = malloc(sizeof(char) * size);
+	for (; i < size; i++)
+		s[i] = c;
 	return (s);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int a = 0;
+	char *s;
+	int b = 0;
+
+	if (str == '\0')
+		return ('\0');
+
+	for (; str[a] != '\0'; a++)
+	{
+	}
+
+	s = malloc(sizeof(char) * a);
+	for (;b <= a; b++)
+		s[b] = str[b];
+	return (s);
+}

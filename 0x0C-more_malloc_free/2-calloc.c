f4961546157bcb059a330bae72b9ff5c0daddbfd
afarizap <afarizap@gmail.com>
2020-03-05 20:38:51 +0000

    asd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *s;
 	unsigned int x = 0;
 
-	if ((nmemb || size) == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
 	if (s == NULL)
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		free(s);
 		return (NULL);
 	}
-	for (; x < size; x++)
+	for (; x < size * nmemb; x++)
 		s[x] = 0;
        	return (s);
 }

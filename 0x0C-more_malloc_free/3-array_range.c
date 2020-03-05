bddf2b4e93d5a881a3d434e8a28c9b976c59d0d2
afarizap <afarizap@gmail.com>
2020-03-05 21:11:34 +0000

    asd

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - prints buffer in hexa
+ * @min: min number
+ * @max: max number
+ * Return: string.
+ */
+int *array_range(int min, int max)
+{
+	int *s, a, x = 0;
+
+	if (min > max)
+		return (NULL);
+	a = max - min;
+	s = malloc(sizeof(int) * a + 1);
+	if (s == NULL)
+		return (NULL);
+	for (; min <= max; x++, min++)
+		s[x] = min;
+	return (s);
+}

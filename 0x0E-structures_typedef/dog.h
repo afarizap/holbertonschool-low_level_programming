5638a0db684e6dd1f9ae2e2d0b99b1e428109160
afarizap <afarizap@gmail.com>
2020-03-09 23:19:26 +0000

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef PERRI
+#define PERRI
+/**
+ *struct dog - perro
+ *@name: nombre del perro
+ *@age: edad del perro
+ *@owner: dueño del perro
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif

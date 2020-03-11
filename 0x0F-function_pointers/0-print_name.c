60103237ff722837c49b0e03b16b2d10a70977f1
afarizap <afarizap@gmail.com>
2020-03-11 21:10:50 +0000

    [betty] coments

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name
  * @name: name of the person
- *
+ * @f: string with print info
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))

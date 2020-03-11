aed486edc08f87cf2128fcf5646b38f49585938b
afarizap <afarizap@gmail.com>
2020-03-11 22:58:11 +0000

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,8 +15,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && cmp && size)
 		for (i = 0; i < size; i++)
 		{
-			if (size <= 0)
-				return (-1);
 			if (cmp(array[i]) != 0)
 				return (i);
 		}

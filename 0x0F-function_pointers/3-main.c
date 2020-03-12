7aed75366c45c012c813e0b8e2a9624886b81d7b
afarizap <afarizap@gmail.com>
2020-03-12 23:38:10 +0000

    final 2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,6 +21,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	s = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
@@ -29,11 +34,6 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(99);
-	}
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(100);
 	}
 	printf("%d\n", res);
 	return (0);

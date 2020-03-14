85b83e335f5c2b0be97e42d34597abd228f8e434
afarizap <afarizap@gmail.com>
2020-03-14 19:33:54 +0000

    betty

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,13 +3,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+/**
+ * struct estructura - estructura de numeros
+ * @c: variable char type
+ * @p: function pointer void type
+ */
 typedef struct estructura
 {
 	char *c;
 	void (*p)(va_list list);
 } struct_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif

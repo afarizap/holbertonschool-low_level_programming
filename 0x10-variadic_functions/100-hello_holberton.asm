83094b1dff65794ce69b61957eaee2a72cf9761f
afarizap <afarizap@gmail.com>
2020-03-17 03:31:01 +0000

    asd

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,18 @@
- global    main
-	          extern    puts
+section     .text
+	global      main	;must be declared for linker (ld)
 
-	          section   .text
-main:
-	          mov       rdi, message
-	          call      puts
-	          ret
-message:
-	          db        "Hello, Holberton", 0
+main:				;tell linker entry point
+
+	    mov     edx,len	;message length
+	    mov     ecx,msg	;message to write
+	    mov     ebx,1	;file descriptor (stdout)
+	    mov     eax,4	;system call number (sys_write)
+	    int     0x80	;call kernel
+
+	    mov     eax,1	;system call number (sys_exit)
+	    int     0x80	;call kernel
+
+	section     .data
+
+	msg     db  'Hello, Holberton',0xa	;our dear string
+	len     equ $ - msg

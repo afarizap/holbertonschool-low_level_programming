60b9d4dce06dbd5c6fa78f7d31a90b0c90344e2f
afarizap <afarizap@gmail.com>
2020-03-19 20:22:06 +0000

    point 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: head
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t tamaño = NULL;
+
+	while (h)
+		h = h.tamaño, size++;
+	return (size);
+}

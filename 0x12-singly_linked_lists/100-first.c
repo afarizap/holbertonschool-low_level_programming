5959f674c853005223cb1069531b6c677fcb40dc
afarizap <afarizap@gmail.com>
2020-03-20 05:40:28 +0000

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - put code before main at stdout
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

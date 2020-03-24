6de5e73005a49b19459a755b21495ffeab6ae78c
afarizap <afarizap@gmail.com>
2020-03-24 12:00:22 +0000

    fix betty and 0 case

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,13 +2,13 @@
 
 /**
  * listint_len - returns the number of elements in a linked listint_t list.
- *
+ * @h: pointer h
  * Return: n
  */
 
 size_t listint_len(const listint_t *h)
 {
-	unsigned int n;
+	unsigned int n = 0;
 
 	while (h)
 	{

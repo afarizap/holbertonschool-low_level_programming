fe63b96bf84f19c06f147c4a16a4a5b3dba036a7
afarizap <afarizap@gmail.com>
2020-03-24 15:33:21 +0000

    fix null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,19 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *aux;
+	//listint_t *aux;
 	listint_t *aux2;
 
 	if (head == NULL)
 		return;
-	aux = *head;
+	//aux = *head;
 	*head = NULL;
 	while (aux)
 	{
-		aux2 = aux;
-		aux = aux->next;
+		//	aux2 = aux;
+		aux2 = head;
+		//aux = aux->next;
+		head = head->next;
 		free(aux2);
 	}
 }

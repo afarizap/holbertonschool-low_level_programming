7e3bd8e63426ddbcfeb776c802b7eb3b468653cf
afarizap <afarizap@gmail.com>
2020-03-25 04:00:28 +0000

    fix null

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,11 +11,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int n = 0;
 	listint_t *aux, *aux2;
 
+	if (!head || !*head)
+		return (-1);
 	aux = *head;
+	if (index == 0)
+	{
+		*head = (**head).next;
+		free(aux);
+		return (1);
+	}
 	while (n + 1 < index)
 	{
 		aux = aux->next;
-		i++;
+		n++;
 	}
 	aux2 = aux->next;
 	aux->next = aux2->next;

d3f60c581ea2daa13b86a6ba47028cccd92e087d
afarizap <afarizap@gmail.com>
2020-03-26 21:04:30 +0000

    add mains

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: head of the linked list
+ * Return: always 0.
+ */
+listint_t *reverse_listint(listint_t **head)
+{

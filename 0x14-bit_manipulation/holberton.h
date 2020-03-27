0fa582f09440cf1665d8e1941ac84152360ec654
afarizap <afarizap@gmail.com>
2020-03-27 02:54:16 +0000

    header update

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <unistd.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif

c76702389dc72d29abf8a1488f01409f4a78e9e4
afarizap <afarizap@gmail.com>
2020-05-02 01:48:35 +0000

    add node counter(0 without print)

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: head of dlinked list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int i = 0;
+
+	for (;h; i++)
+		h = (*h).next;
+	return (i);
+}

0773248ae890f0dbc779b215a2ff33e9f7a3b789
afarizap <afarizap@gmail.com>
2020-05-02 16:46:46 +0000

    fix structure

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+
 	if (!new)
-		return(NULL);
+		return (NULL);
+
 	(*new).n = n;
 	(*new).next = *head;
 	(*new).prev = NULL;
+
+	if (head)
+		(**head).prev = new;
 	*head = new;
 
 	return (new);

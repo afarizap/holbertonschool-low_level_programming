726b6e8871cac4a57f8e96987513885b0a73cf98
afarizap <afarizap@gmail.com>
2020-05-02 17:06:33 +0000

    refac

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,25 +2,22 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: head of the dllist
- * @n: number to add to the dllist
+ * @head: head of dllist
+ * @n: number added
  * Return: the address of the new element, or NULL if it failed
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
-
+	new = malloc(sizeof(dlistint_t))
 	if (!new)
 		return (NULL);
-
 	(*new).n = n;
-	(*new).next = *head;
 	(*new).prev = NULL;
+	(*new).next = *head;
 
-	if (head)
+	if (*head)
 		(**head).prev = new;
 	*head = new;
 

fdc75f8447ac2fb2f89eeb562953f0a71a37caf2
afarizap <afarizap@gmail.com>
2020-05-02 16:17:22 +0000

    fix returns addnodedllist, new->prev = NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-
+	if (!new)
+		return(NULL);
 	(*new).n = n;
 	(*new).next = *head;
 	(*new).prev = NULL;
 	*head = new;
 
-	return (*head);
+	return (new);
 }

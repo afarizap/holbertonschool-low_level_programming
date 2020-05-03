24a6cb6eb8c4826712671a6e1e16f2e4dc370680
afarizap <afarizap@gmail.com>
2020-05-03 15:24:04 +0000

    add getnode and proto

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: first node of the dllist
+ * @index: index of the node, starting from 0
+ * Return: NULL if node does not exist, the list if does
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t i = 0;
+	dlistint_t *get;
+
+	get = malloc(sizeof(dlistint_t));
+	for (; i < index; i++)
+		head = (*head).next;
+	(*get).n = (*head).n;
+	return (get);
+}

e970280cf00b59782bd5dfe0a958722cd3443410
afarizap <afarizap@gmail.com>
2020-05-04 02:53:13 +0000

    fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,24 +7,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	if ((*head).prev != NULL)
+	dlistint_t *aux = head, *aux2 = NULL;
+
+	while(aux)
 	{
-		while ((*head).prev != NULL)
-		{
-			head = (*head).prev;
-			free((*head).next);
-		}
-		free(head);
+		aux2 = aux;
+		aux = (*aux).next;
+		free (aux2);
 	}
-	else if ((*head).next != NULL)
-	{
-		while ((*head).next != NULL)
-		{
-			head = (*head).next;
-			free((*head).prev);
-		}
-		free(head);
-	}
-	else
-		free(head);
 }

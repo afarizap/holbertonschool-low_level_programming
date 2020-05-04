ed234b5abb9fd0c85ba7036c3345d04088bac4ab
afarizap <afarizap@gmail.com>
2020-05-04 04:22:56 +0000

    add delete and proto

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)

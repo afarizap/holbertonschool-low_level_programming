c793af87f2ea5a10bb4130b4d2561413a5e604f8
afarizap <afarizap@gmail.com>
2020-05-05 01:08:12 +0000

    fix script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fpic *.c | gcc -shared -o liball.so *.o
+gcc -Wall -pedantic -Werror -Wextra -c -fpic *.c
+gcc -Wall -pedantic -Werror -Wextra -shared -o liball.so *.o

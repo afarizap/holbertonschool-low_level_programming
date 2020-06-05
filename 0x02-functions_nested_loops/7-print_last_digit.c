6cf59851eee7731e1271b1bdadf0e41cef1a11a1
afarizap <afarizap@gmail.com>
2020-06-05 16:27:23 +0000

    _putchar prints before printf

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *print_last_digit - function to print last digit of a number.
- *
+ * print_last_digit - function to print last digit of a number.
+ * @c: number
  * Return: Always 0.
  */
 
 int print_last_digit(int c)
 {
-	c = c % 10;
-	printf("%d", c);
-	return (c);
+	int lastdigit;
+
+	lastdigit = abs(c) % 10;
+	_putchar('0' + lastdigit);
+	return (lastdigit);
 }

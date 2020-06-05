bea60faed166404521221efd509463097cdaa8aa
afarizap <afarizap@gmail.com>
2020-06-05 17:46:58 +0000

    gj jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,61 +1,27 @@
 #include "holberton.h"
-#include <stdio.h>
-
-
+/**
+ * jack_bauer - print every minute of the day of Jack Bauer 00:00 - 23:59
+ */
 void jack_bauer(void)
 {
-	int hora1 = '0';
-	int hora2 = '0';
-	int min1 = '0';
-	int min2 = '0';
+	char m1 = '0', m2 = '0', h1 = '0', h2 = '0';
 
-	while (hora1 < '2' || hora2 < '4')
+	for (; h1 < '4' || h2 < '2'; m1++)
 	{
-		_putchar(hora1);
-		_putchar(hora2);
+		if (m1 > '9')
+			m1 = '0', m2 += 1;
+		if (m2 == '6')
+			m2 = '0', h1 += 1;
+		if (h1 > '9')
+			h1 = '0', h2 += 1;
+		if (h1 == '4' && h2 == '2')
+			break;
+		_putchar(h2);
+		_putchar(h1);
 		_putchar(':');
-		_putchar(min1);
-		_putchar(min2);
+		_putchar(m2);
+		_putchar(m1);
 		_putchar('\n');
-		if (hora2 == '9' && min1 == '5' && min2 == '9')
-		{
-			hora1++;
-			hora2 = '0';
-			min1 = '0';
-			min2 = '0';
-			_putchar(hora1);
-			_putchar(hora2);
-			_putchar(':');
-			_putchar(min1);
-			_putchar(min2);
-			_putchar('\n');
-		}
-		if (min1 == '5' && min2 == '9')
-		{
-			hora2++;
-			min1 = '0';
-			min2 = '0';
-			if (hora1 != '2' && hora2 != '4')
-			{
-				_putchar(hora1);
-				_putchar(hora2);
-				_putchar(':');
-				_putchar(min1);
-				_putchar(min2);
-				_putchar('\n');
-			}
-		}
-		if (min2 == '9')
-		{
-			min1++;
-			min2 = '0';
-			_putchar(hora1);
-			_putchar(hora2);
-			_putchar(':');
-			_putchar(min1);
-			_putchar(min2);
-			_putchar('\n');
-		}
-		min2++;
 	}
+
 }

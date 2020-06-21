8c8b9aa0d56a920f6cb301db92125728d33e83c8
afarizap <afarizap@gmail.com>
2020-06-21 15:48:11 +0000

    fix header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -6,8 +6,15 @@
 #include <stddef.h>
 #include <unistd.h>
 
-int _putchar(char c);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-
+int _putchar(char c);
 #endif

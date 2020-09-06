88443438a3fe4b8cbdea8dbf6fb77a7307d74d24
afarizap <afarizap@gmail.com>
2020-09-06 22:37:20 +0000

    add return at end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,29 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t)), *aux = *h;
+	dlistint_t *aux = *h, *new = malloc(sizeof(dlistint_t));
 
 	if (!new)
 		return (NULL);
 	(*new).n = n;
-	(*new).prev = NULL;
 	(*new).next = NULL;
+	(*new).prev = NULL;
+
+
 	if (idx == 0)
 		if (!*h)
-		{
 			*h = new;
-			return (new);
-		}
 		else
-		{
-			(*new).next = *h;
-			(*aux).prev = new;
-			return (new);
-		}
+			(*new).next = *h, (**h).prev = new, *h = new;
 	else
 	{
-		for (; idx > 0; idx--)
-			if ((*aux).next != NULL)
-				aux = (*aux).next;
-		if (idx != 0)
-			return (NULL);
-		(*new).prev = aux;
-		(*new).next = (*aux).next;
+		if (idx > 0)
+			while ((*aux).next)
+				aux = (*aux).next, i--;
+		if ((*aux).next)
+			(*new).next = NULL;
 		(*aux).next = new;
-		if ((*aux).next == NULL)
-			return (new);
-		aux = (*aux).next;
-		aux = (*aux).next;
-		(*aux).prev = new;
-		return (new);
+		(*aux).prev = aux;
 	}
+	return (new);
 }

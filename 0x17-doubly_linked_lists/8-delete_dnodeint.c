969e37d2efb6902d7581263cb8c4c069fbb9ab8f
afarizap <afarizap@gmail.com>
2020-09-13 01:11:13 +0000

    first try

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,8 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * delete_dnodeint_at_index - deletes the node at index index of a dllist
+ * @head: start of the node
+ * @index: indext of list to remove
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *aux = *head, *auxp = *head;
+	unsigned int temp = 0;
+
+	while (aux->next)
+		temp++, aux = aux->next;
+	if (index > temp)
+		return (-1);
+	temp = 0;
+	while (index != temp)
+		temp++, auxp = auxp->next;
+	aux = *head;
+	if (aux->prev)
+	{
+		aux = aux->prev;
+		aux->next = auxp->next;
+		auxp->next->prev = aux;
+	}
+	return (1);
+}
